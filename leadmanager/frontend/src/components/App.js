213e1dccd740e8d86d7c673963a20f9779867636
rouro02 <rayenghali02@gmail.com>
2021-03-15 22:27:31 +0100

    vid 3

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -4,19 +4,21 @@ import React, { Component , Fragment} from 'react';
 import Header from './layout/Header';
 import Dashboard from './leads/Dashboard';
 
+import {Provider} from 'react-redux';
+import store from '../store';
 
 class App extends Component {
 
     render() {
-        return( 
-            <Fragment>
-                <Header />
-                <div className="container" >
-                    <Dashboard/>
-                </div>
-            </Fragment>
-
-        
+        return(
+            <Provider store={store}>
+                <Fragment >
+                    <Header />
+                    <div className="container" >
+                        <Dashboard/>
+                    </div>
+                </Fragment>
+            </Provider>
         )
         }
     }
